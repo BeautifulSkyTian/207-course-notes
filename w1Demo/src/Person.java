306535aca88875268e51f1b15fa258a799d5411d
Tianqi Pan <pantianqi666@gmail.com>
2026-09-14 13:53:25 -0400

    added week 1 demo

---
+++ w1Demo/src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+
+    private String name;
+
+    public Person(String n){
+        this.name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+}
