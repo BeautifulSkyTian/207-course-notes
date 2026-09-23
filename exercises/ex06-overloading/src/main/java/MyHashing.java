edebbcc18bdf61885426deebdcdc8cdf91b6dbc1
Tianqi Pan <pantianqi666@gmail.com>
2026-09-23 13:44:37 -0400

    exercise 6

--- exercises/ex06-overloading/src/main/java/MyHashing.java
+++ exercises/ex06-overloading/src/main/java/MyHashing.java
@@ -28,7 +28,7 @@ public class MyHashing {
    * @param seed the initial seed value
    */
   public MyHashing(int seed) {
-    // TODO: store the parameter in this object's seed field.
+    this.seed = seed;
   }
 
   /**
@@ -38,8 +38,9 @@ public class MyHashing {
    * @return the seed value from before this call
    */
   public int hash(int value) {
-    // TODO
-    return 0;
+    int old_seed = this.seed;
+    this.seed = value;
+    return old_seed;
   }
 
   /**
@@ -51,8 +52,9 @@ public class MyHashing {
    * @return (previous seed + value) % MODULO
    */
   public int hash(char value) {
-    // TODO
-    return 0;
+    int ans = (this.seed + value) % MODULO;
+    this.seed = value;
+    return ans;
   }
 
   /**
@@ -64,7 +66,11 @@ public class MyHashing {
    * @return the sum of the characters' numeric codes
    */
   public static int hash(String value) {
-    // TODO: String.toCharArray() may help.
-    return 0;
+
+    int sum = 0;
+    for(int i = 0; i<value.length(); i++){
+        sum += value.charAt(i);
+    }
+    return sum;
   }
 }
